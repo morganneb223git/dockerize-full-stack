c8b0eb44f747ebb1d641bec8b0cc45d13d566e45
Morganne Brown <morganneb223@gmail.com>
2024-02-15 23:10:53 -0600

    initial commit

---
+++ server/src/database.js
@@ -0,0 +1,25 @@
+/* 
+const mongoose = require('mongoose');
+const User = require('./models/user.model');
+
+const connection = 'mongodb://localhost:27017/dockerize-me';
+
+const connectDb = () => {
+  return mongoose.connect(connection);
+};
+
+module.exports = connectDb; 
+*/
+
+const mongoose = require('mongoose');
+
+const connection = 'mongodb://mongo:27017/dockerize-me'; 
+
+const connectDb = () => {
+  return mongoose.connect(connection, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+};
+
+module.exports = connectDb;
